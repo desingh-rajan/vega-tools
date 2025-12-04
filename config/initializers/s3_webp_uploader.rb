2a3d8a49ff2fd4bc7f5b647254cef87edbc555b8
Desingh <desinghrajan@gmail.com>
2025-12-04 21:28:43 +0530

    fix: Map Rails env names to S3 folder names (dev/prod)

--- config/initializers/s3_webp_uploader.rb
+++ config/initializers/s3_webp_uploader.rb
@@ -1,7 +1,10 @@
 S3WebpUploader.configure do |config|
   config.bucket = "tstack-desingh"
   config.region = "ap-south-1"
-  config.prefix = "vega-tools/#{Rails.env}/images"
+  
+  # Map Rails environments to S3 folder names
+  env_folder = { "development" => "dev", "production" => "prod", "test" => "test" }
+  config.prefix = "vega-tools/#{env_folder.fetch(Rails.env, Rails.env)}/images"
 
   # AWS credentials auto-loaded from Rails credentials (aws.access_key_id, aws.secret_access_key)
 end
