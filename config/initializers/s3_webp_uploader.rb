6347976234addd83bc67e8be591735193147ad70
Desingh <desinghrajan@gmail.com>
2025-12-04 22:01:42 +0530

    Remove unused files: json_display_helper, image_converter, active_storage_key

--- config/initializers/s3_webp_uploader.rb
+++ config/initializers/s3_webp_uploader.rb
@@ -1,7 +1,7 @@
 S3WebpUploader.configure do |config|
   config.bucket = ENV.fetch("S3_BUCKET", "tstack-desingh")
   config.region = ENV.fetch("S3_REGION", "ap-south-1")
-  
+
   # Map Rails environments to S3 folder names
   env_folder = { "development" => "dev", "production" => "prod", "test" => "test" }
   app_name = ENV.fetch("S3_APP_NAME", "vega-tools")
